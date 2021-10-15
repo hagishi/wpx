76f151811ad189b41434bb270e80e11d2378e656
hagishi <fujiyama@kakumei.jp>
2021-10-15 09:19:52 +0900

    hello

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/helloworld', function() {
+    return ['hello'];
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });
